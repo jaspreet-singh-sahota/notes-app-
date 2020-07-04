e364c13fbc57fd9ffca7ceae6aa538e7072b7d44
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-04 10:31:56 +0530

    Show todos list via DOM

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 console.log('initial setup done Notes App')
 
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
 const body = document.querySelector('body')
 const paragraph = document.createElement('p')
 paragraph.textContent = "This is the new paragraph add via DOM"
