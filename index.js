1d70fcd39e7807d78450c9dc964cd30ad87ef2a7
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-04 09:54:13 +0530

    Add p tag using DOM

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('initial setup done')
+console.log('initial setup done Notes App')
+
+const body = document.querySelector('body')
+const paragraph = document.createElement('p')
+paragraph.textContent = "This is the new paragraph add via DOM"
+
+body.append(paragraph)
+
